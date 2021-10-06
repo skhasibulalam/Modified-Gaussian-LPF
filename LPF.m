8fa1a18cc4e2695fafd6e12f7435a3e652fae724
Sk Hasibul Alam <sk.hasibul.alam@gmail.com>
2021-10-06 06:12:04 +0600

    Update LPF.m

--- LPF.m
+++ LPF.m
@@ -1,73 +1,70 @@
-clc
-clear all
-
-a = imread('bsec.jpg');% Use your desired Image file in this line.
-
-level=1;% Number of times each "FOR-LOOP" you want to run.
+a=imread('actual.jpg');% Use your desired Image file in this line.
+level=2;% Number of times each "FOR-LOOP" you want to run.
 % By the way, there are 2 FOR-LOOPs in this code.
 
-% Now we'll declare our Gaussian Filter length.
-x=[-10:1:10];
-y=[-10:1:10];
-fil_r=size(x,2);
-fil_c=size(y,2);
-variance=3;% It is actually: 2 x (sigma)^2.
-gaussi=zeros(fil_r,fil_c);
-for i=1:fil_r
-    for j=1:fil_c
-        expon=exp(-(x(i)^2+y(j)^2)/(variance));
-        gaussi(i,j)=expon;
+in=double(a);
+x=-15:15;
+y=-15:15;
+sigma=2;
+g=zeros(length(x),length(y));
+for i=1:length(x)
+    for j=1:length(y)
+        g(i,j)=exp(-(x(i)^2+y(j)^2)/(2*sigma^2));
     end
 end
-% Filter is ready. Now we need to normalize, otherwise the output Image
-% will be too white or too black.
-gaussi=gaussi/sum(sum(gaussi));
+g=g/(2*pi*sigma^2);
 
-d_image= double(a);% Convert to "double precision" from "Integer(depth-8)" 
-d_r= size(d_image,1);
-d_c= size(d_image,2);
-dmin= min(d_r,d_c);% Minimum between height & width of that Image
+d_image=double(a);% Convert to "double precision" from "Integer(depth-8)" 
+dmin=min(size(d_image,[1 2]));% Minimum between height & width of that Image
 
 % You simply don't want to downsample a "4x4 matrix" more than 2 times.
-dsamp_level= floor(log2(dmin));
+dsamp_level=floor(log2(dmin));
 % This will automtically override if you're wrong in "line 6" before.
-fin_level= min(dsamp_level,level);
-
-image_cell=cell(fin_level+1,1);
-image_cell{1}=d_image;
+fin_level=min(dsamp_level,level);
 
 for i=1:fin_level
-    filtered_r = conv2fft(d_image(:,:,1),gaussi);% Red Layer
-    filtered_g = conv2fft(d_image(:,:,2),gaussi);% Green Layer
-    filtered_b = conv2fft(d_image(:,:,3),gaussi);% Blue Layer
-    dsamped_r = dyaddown(filtered_r,1,'m');% Downsampling R
-    dsamped_g = dyaddown(filtered_g,1,'m');% Downsampling G
-    dsamped_b = dyaddown(filtered_b,1,'m');% Downsampling B
+    filtered_r = myfun(d_image(:,:,1),g);
+    filtered_g = myfun(d_image(:,:,2),g);
+    filtered_b = myfun(d_image(:,:,3),g);
+    dsamped_r = dyaddown(filtered_r,1,'m');
+    dsamped_g = dyaddown(filtered_g,1,'m');
+    dsamped_b = dyaddown(filtered_b,1,'m');
     clear recur_down;
-    % Now we'll recombine R, G, B layer.
     recur_down(:,:,1) = dsamped_r;
     recur_down(:,:,2) = dsamped_g;
     recur_down(:,:,3) = dsamped_b;
-    image_cell{i+1} = recur_down;
     d_image = recur_down;
 end
 % 1st "FOR-LOOP" is done, now we're entering the 2nd "FOR-LOOP".
 for i=1:fin_level
-    usamped_r = dyadup(d_image(:,:,1),0,'m');% Upsampling R
-    usamped_g = dyadup(d_image(:,:,2),0,'m');% Upsampling G
-    usamped_b = dyadup(d_image(:,:,3),0,'m');% Upsampling B
-    % Multiplying with a factor "4", otherwise the Image would be faded.
-    % You may use "3.8" or "4.2" instead; it's upto you.
-    filtered_r = 4*conv2fft(usamped_r,gaussi);
-    filtered_g = 4*conv2fft(usamped_g,gaussi);
-    filtered_b = 4*conv2fft(usamped_b,gaussi);
+    usamped_r = dyadup(d_image(:,:,1),1,'m');
+    usamped_g = dyadup(d_image(:,:,2),1,'m');
+    usamped_b = dyadup(d_image(:,:,3),1,'m');
+    usamped_r = usamped_r(2:size(usamped_r,1),2:size(usamped_r,2));
+    usamped_g = usamped_g(2:size(usamped_g,1),2:size(usamped_g,2));
+    usamped_b = usamped_b(2:size(usamped_b,1),2:size(usamped_b,2));
+    filtered_r = 4*myfun(usamped_r,g);
+    filtered_g = 4*myfun(usamped_g,g);
+    filtered_b = 4*myfun(usamped_b,g);
     clear d_image;
-    % Now we'll recombine R, G, B layer.
     d_image(:,:,1) = filtered_r;
     d_image(:,:,2) = filtered_g;
     d_image(:,:,3) = filtered_b;
 end
 
 out = uint8(d_image);
-% Now, we'll save the filtered Image.
-imwrite(out, 'comp.jpg');
+imwrite(out,'compressed.jpg');
+
+pin=sum(sum(sum(in.^2)));
+pnoi=sum(sum(sum((in-d_image).^2)));
+snr=pin/pnoi
+
+function [out] = myfun(a,b)
+[ax,ay]=size(a,[1 2]);
+[bx,by]=size(b,[1 2]);
+out=(ifft2(fft2(a,ax+bx-1,ay+by-1).*fft2(b,ax+bx-1,ay+by-1)));
+px=((bx-1)+mod((bx-1),2))/2;
+py=((by-1)+mod((by-1),2))/2;
+out=out(px+1:px+ax,py+1:py+ay);
+return;
+end
